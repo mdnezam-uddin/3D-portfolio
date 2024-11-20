296aab47a66a912f8cae319bc108edaaa788bf2f
MD NEZAM UDDIN <154408849+mdnezam-uddin@users.noreply.github.com>
2024-11-20 21:39:16 +0800

    complete About section

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,12 @@ import {
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+      >
         <div className="relative z-0 bg-primary">
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
             <Navbar />
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
 
-const About = () => {
-  return (
-    <div>About</div>
+import {motion}from 'framer-motion'
+import {styles} from "../style"
+import { services } from '../constants'
+import {fadeIn,textVariant} from "../utils/motion"
+
+import { Tilt } from 'react-tilt'
+import { SectionWrapper } from '../hoc'
+
+const ServiceCard = ({title,icon, index}) => {
+  return(
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+      variants={fadeIn("right","spring",0.5 * index, 0.75)}
+      className='w-full green-pink-gradient rounded-[20px] p-[1px] shadow-card'
+      >
+        <div
+        options={{ max:45, scale:1,speed:450}}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center' 
+        >
+          <img src={icon} alt={title} className='w-20 h-20'/>
+          <h3 className='text-center text-white text-[20px] font-bold'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div
+      variants={textVariant()}
+      >
+        <p className={styles.sectionSubText}>Introduction</p>
+        <p className={styles.sectionHeadText}>Overview</p>
+      </motion.div>
+
+      <motion.p
+      variants={fadeIn("","",0.5, 1)}
+      className='text-[17px] leading-[30px] mt-4 max-w-3xl text-secondary'
+      >
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service}/>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about")
