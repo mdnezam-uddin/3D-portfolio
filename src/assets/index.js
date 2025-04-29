e5e8614b7759fe13e1b8b1515008848b55f74305
MD NEZAM UDDIN <154408849+mdnezam-uddin@users.noreply.github.com>
2025-04-29 17:08:02 +0800

    v.1.0

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,14 +21,7 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import papalazius from "./company/papalazius.png";
 import suncaper from "./company/suncaper.png";
 
@@ -58,13 +51,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
   papalazius,
   suncaper,
   whatsappclone,
